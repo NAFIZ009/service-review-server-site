3472d041383d5b89c04cb1c9588cd817b33298bb
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-11-11 23:41:38 +0600

    sec

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const app = express();
 const port=process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustervideowalah.6bieeyw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
