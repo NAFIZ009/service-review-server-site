f398ee422edd8fe54aac717067c64b7302b4169e
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-11-14 00:08:50 +0600

    update review

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port=process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustervideowalah.6bieeyw.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +25,28 @@ app.get('/services',async (req,res)=>{
             res.send(services);
         }
         
+    }catch{
+
+    }
+});
+app.get('/services/:email',async (req,res)=>{
+    try{
+        const data=client.db('videoWalah').collection('services');
+
+        const cursor=await data.find({mail:req.params.email});
+        const services=await cursor.toArray();
+        res.send(services);
+        
+    }catch{
+
+    }
+});
+app.post('/services',async (req,res)=>{
+    try{
+        const data=client.db('videoWalah').collection('services');
+        const service=req.body;
+        const cursor=await data.insertOne(service);
+        res.send(cursor);
     }catch{
 
     }
@@ -60,6 +84,26 @@ app.delete('/reviews/:id',async (req,res)=>{
         const id=req.params.id;
         const query={_id: ObjectId(id)};
         const result=await data.deleteOne(query);
+        res.send(result);  
+    }catch{
+
+    }
+});
+
+
+app.put('/reviews/:id',async (req,res)=>{
+    try{
+        const data=client.db('videoWalah').collection('review');
+        const id=req.params.id;
+        const body=req.body;
+        // console.log(body);
+        const query={_id: ObjectId(id)};
+        const updatedData={
+            $set:{
+                review:body.rev
+            }
+        }
+        const result=await data.updateOne(query,updatedData);
         res.send(result);  
     }catch{
 
